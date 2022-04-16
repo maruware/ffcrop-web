6d841f694b20c2d53db01399585d08cd40c7cf3e
maruware <me@maruware.com>
2022-04-16 21:48:49 +0900

    Fix vite config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/ffcrop-web/" : "",
   plugins: [
     react(),
     {
